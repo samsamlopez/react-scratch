0246392720e7a409132f9c6144e8b58a5f7c12f8
Samuel Lopez <samuel.lopez@boom.camp>
2020-01-07 09:40:18 +0800

    React from scrath

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import TestMainView from './views/test-main-view/TestMainView'
+
+function App() {
+  // Here you will use context api/router etc.
+}
+
+export default TestMainView;
